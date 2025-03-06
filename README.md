957ba515fa1bd4e8de87642fd8a309c3249032cb
mukarrom <mukarrom21@gmail.com>
2025-03-06 15:33:50 +0600

    markdown updated

--- README.md
+++ README.md
@@ -1,39 +1,45 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# color_size_picker
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/tools/pub/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/to/develop-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+A Flutter package for selecting colors and sizes in a user-friendly way.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+- Allows users to pick colors from a predefined list.
+- Supports custom color palettes.
+- Provides a callback mechanism to handle color selection.
+- Handles edge cases like empty color lists gracefully.
 
-## Getting started
+## Installation
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+Add the following dependency to your `pubspec.yaml` file:
 
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
-```dart
-const like = 'sample';
+```yaml
+dependencies:
+  color_size_picker: ^0.0.1
 ```
 
-## Additional information
+Then, run flutter pub get to install the package.
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+## Usage
+Import the package in your Dart file:
+
+```dart
+import 'package:color_size_picker/color_size_picker.dart';
+```
+
+Use the ColorPickerWidget in your app:
+
+```dart
+ColorPickerWidget(
+  colors: ["Green", "Red", "Blue", "Yellow"],
+  onChangeColor: (selectedColor) {
+    print("Selected color: $selectedColor");
+  },
+)
+```
+
+Contributing
+Contributions are welcome! Please open an issue or submit a pull request on GitHub.
+
+License
+This project is licensed under the MIT License. See the LICENSE file for details.
--- lib/src/color_picker.dart
+++ lib/src/color_picker.dart
@@ -10,6 +10,7 @@ class ColorPickerWidget extends StatefulWidget {
 
   final List<String> colors;
   final void Function(String)? onChangeColor;
+
   @override
   State<ColorPickerWidget> createState() => _ColorPickerWidgetState();
 }
@@ -23,48 +24,52 @@ class _ColorPickerWidgetState extends State<ColorPickerWidget> {
     for (String color in widget.colors) {
       _colors[color] = AppColors.colors[color] ?? Colors.black;
     }
-    if (widget.colors.isNotEmpty) {
-      _selectedColor = _colors[widget.colors.first] ?? AppColors.themeColor;
-    } else {
-      _selectedColor = AppColors.themeColor;
-    }
+    _selectedColor = widget.colors.isNotEmpty
+        ? _colors[widget.colors.first] ?? AppColors.themeColor
+        : AppColors.themeColor;
     super.initState();
+  }
+
+  Widget _buildColorCircle(MapEntry<String, Color> entry) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedColor = entry.value;
+        });
+        widget.onChangeColor?.call(entry.key);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: entry.value,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: entry.value == Colors.white
+                ? AppColors.themeColor
+                : _selectedColor == entry.value
+                    ? Colors.black
+                    : Colors.transparent,
+            width: _selectedColor == entry.value ? 2.0 : 0.0,
+          ),
+        ),
+        width: 28,
+        height: 28,
+        child: Icon(
+          Icons.check,
+          color: _selectedColor != entry.value
+              ? Colors.transparent
+              : entry.value == Colors.white
+                  ? AppColors.themeColor
+                  : Colors.white,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Wrap(
       spacing: 12,
-      children: _colors.entries.map((entry) {
-        return GestureDetector(
-          onTap: () {
-            _selectedColor = entry.value;
-            widget.onChangeColor?.call(entry.key);
-            setState(() {});
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: entry.value,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: entry.value == Colors.white
-                    ? AppColors.themeColor
-                    : Colors.transparent,
-              ),
-            ),
-            width: 28,
-            height: 28,
-            child: Icon(
-              Icons.check,
-              color: _selectedColor != entry.value
-                  ? Colors.transparent
-                  : entry.value == Colors.white
-                      ? AppColors.themeColor
-                      : Colors.white,
-            ),
-          ),
-        );
-      }).toList(),
+      children: _colors.entries.map(_buildColorCircle).toList(),
     );
   }
 }
